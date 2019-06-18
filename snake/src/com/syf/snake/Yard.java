c1ca5aba6ea5fe3a699d51b6c42662263bdede4a
cloudy <syf0607@163.com>
2019-06-18 15:52:25 +0800

    new

---
+++ snake/src/com/syf/snake/Yard.java
@@ -0,0 +1,14 @@
+package com.syf.snake;
+
+import java.awt.*;
+
+/**
+ * @ Author :cloudy
+ * @ Date   :Created in 15:17 2019/6/18
+ * @ Description: 院墙（游戏的边界)
+ */
+public class Yard extends Frame {
+    public static void main(String[] args) {
+
+    }
+}
